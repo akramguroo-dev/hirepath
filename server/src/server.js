4391808e7b545c275c0ca2d9cabbf96cabc31980
akramguroo-dev <akramguroo.dev@gmail.com>
2026-03-04 17:57:46 +0530

    Add auth routes for register and login

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 require('dotenv').config();
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth');
 
 const app = express();
+
+app.use('/api/auth', authRoutes);
 
 app.get('/api/test', (req, res) => {
   res.json({ message: "API is working" });
