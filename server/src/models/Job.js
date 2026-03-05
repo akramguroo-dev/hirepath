a77d5f152a6b5592129c8c8f9db2235eac5a0a7c
akramguroo-dev <akramguroo.dev@gmail.com>
2026-03-05 18:14:46 +0530

    Add Job model schema

---
+++ server/src/models/Job.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const jobSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ['internship', 'full-time', 'part-time'],
+    required: true,
+  },
+  salary: {
+    type: Number,
+  },
+  duration: {
+    type: String,
+  },
+  posted_by: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['open', 'closed'],
+    default: 'open',
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Job', jobSchema);
