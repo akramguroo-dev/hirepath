7e3702277bfd056552d8b7dc3d262e40346719fb
akramguroo-dev <akramguroo.dev@gmail.com>
2026-03-07 07:27:49 +0530

    Add Application model schema

---
+++ server/src/models/Application.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const applicationModel = new Schema({
+  job_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Job', 
+    required: true,
+  },
+  student_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'accepted', 'rejected'],
+    default: 'pending'
+  },
+  resume_url: {
+    type: String,
+  },
+  cover_letter: {
+    type: String,
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Application', applicationModel);
