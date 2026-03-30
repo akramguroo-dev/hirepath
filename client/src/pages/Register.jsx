2fbb73cfa05fc68a6afd05a417586d968c05a65d
akramguroo-dev <akramguroo.dev@gmail.com>
2026-03-30 07:01:02 +0530

    feat: register API integration, redirect to login

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,40 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import API from "../api/axios";
+import { useState } from "react";
 
 export default function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await API.post("/auth/register", {
+        name,
+        email,
+        password,
+      });
+      navigate("/login");
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <form className="bg-white shadow rounded px-6 pt-12 pb-6 w-96 max-w-md">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow rounded px-6 pt-12 pb-6 w-96 max-w-md"
+      >
         <h2 className="text-2xl font-bold mb-6 text-center">Register</h2>
         <div className="flex gap-8 mb-6">
-          <div className="text-[#00A5EC] border-b-2 border-[#00A5EC] pb-1 inline-block">Student</div>
+          <div className="text-[#00A5EC] border-b-2 border-[#00A5EC] pb-1 inline-block">
+            Student
+          </div>
           <div className="text-gray-500 pb-1 cursor-not-allowed">Employer</div>
         </div>
         <div className="flex justify-center mb-4">
@@ -14,17 +42,35 @@ export default function Register() {
         </div>
         <label htmlFor="" className="block mb-4">
           Name
-          <input className="border rounded w-full px-3 py-2 mt-2 focus:outline-none focus:border-blue-500" type="text" placeholder="John Doe" />
+          <input
+            onChange={(e) => setName(e.target.value)}
+            className="border rounded w-full px-3 py-2 mt-2 focus:outline-none focus:border-blue-500"
+            type="text"
+            placeholder="John Doe"
+          />
         </label>
         <label htmlFor="" className="block mb-4">
           Email
-          <input className="border rounded w-full px-3 py-2 mt-2 focus:outline-none focus:border-blue-500" type="email" placeholder="john@example.com"/>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            className="border rounded w-full px-3 py-2 mt-2 focus:outline-none focus:border-blue-500"
+            type="email"
+            placeholder="john@example.com"
+          />
         </label>
         <label htmlFor="" className="block mb-3">
           Password
-          <input className="border rounded w-full px-3 py-2 mt-2 focus:outline-none focus:border-blue-500" type="password" placeholder="Must be atleast 6 characters" />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            className="border rounded w-full px-3 py-2 mt-2 focus:outline-none focus:border-blue-500"
+            type="password"
+            placeholder="Must be atleast 6 characters"
+          />
         </label>
-        <button type="submit" className="w-full border rounded bg-[#00A5EC] text-white font-semibold py-2 mb-4 hover:bg-blue-600">
+        <button
+          type="submit"
+          className="w-full border rounded bg-[#00A5EC] text-white font-semibold py-2 mb-4 hover:bg-blue-600 cursor-pointer"
+        >
           Register
         </button>
         <p className="w-full">
