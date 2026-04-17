614240e379ed53830f218fb222e0f250ed89a244
akramguroo-dev <akramguroo.dev@gmail.com>
2026-04-17 15:11:22 +0530

    feat: add protected routes for profile, employer-dashboard, post-job

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,20 +11,45 @@ import Profile from "./pages/Profile";
 import EmployerDashboard from "./pages/EmployerDashboard";
 import EmployerApplicants from "./pages/EmployerApplicants";
 import PostJob from "./pages/PostJob";
+import ProtectedRoute from "./components/ProtectedRoute";
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/employer-dashboard"
+          element={
+            <ProtectedRoute>
+              <EmployerDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/post-job"
+          element={
+            <ProtectedRoute>
+              <PostJob />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/" element={<Home />} />
-        <Route path="/jobs" element={<Jobs/>} />
+        <Route path="/jobs" element={<Jobs />} />
         <Route path="/jobs/:id" element={<JobDetail />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/employer-dashboard" element={<EmployerDashboard />} />
-        <Route path="/employer/jobs/:id/applicants" element={<EmployerApplicants />} />
-        <Route path="/post-job" element={<PostJob />} />
+        <Route
+          path="/employer/jobs/:id/applicants"
+          element={<EmployerApplicants />}
+        />
       </Routes>
       <Footer />
     </>
